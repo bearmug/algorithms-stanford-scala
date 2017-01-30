c020b6bb490f67c94906e21ce450223d6d780dae
bearmug <pavel.fadeev@gmail.com>
2017-01-30 21:19:17 +0300

    #2 refactor recursive algorithm to make it cleaner

--- src/main/scala/org/bearmug/algo/course01/week01/MultiplicationAlgo.scala
+++ src/main/scala/org/bearmug/algo/course01/week01/MultiplicationAlgo.scala
@@ -1,6 +1,6 @@
 package org.bearmug.algo.course01.week01
 
-case class Recursive(val n: String) {
+case class Recursive(n: String) {
 
   def +(other: Recursive): Recursive = Recursive((BigInt(this.n) + BigInt(other.n)).toString())
 
@@ -10,21 +10,19 @@ case class Recursive(val n: String) {
   }
 
   def *(other: Recursive): Recursive = (this.n, other.n) match {
+    case (aR, bR) if aR.length > bR.length => other * this
     case ("", _) => Recursive("0")
-    case (_, "") => Recursive("0")
-    case (thisK, thatK) if thisK.length == 1 => Recursive((thisK.toInt * thatK.toInt).toString)
-    case (thisK, thatK) if thatK.length == 1 => Recursive((thisK.toInt * thatK.toInt).toString)
-    case (thisK, thatK) => {
-      val len = thisK.length max thatK.length
-      val (a, b) = (thisK.substring(0, thisK.length - len / 2), thisK.substring(thisK.length - len / 2))
-      val (c, d) = (thatK.substring(0, thatK.length - len / 2), thatK.substring(thatK.length - len / 2))
+    case (aR, bR) if aR.length == 1 => Recursive((aR.toInt * bR.toInt).toString)
+    case (aR, bR) =>
+      val len = aR.length
+      val (a, b) = (aR.substring(0, aR.length - len / 2), aR.substring(aR.length - len / 2))
+      val (c, d) = (bR.substring(0, bR.length - len / 2), bR.substring(bR.length - len / 2))
       val (ac, ad, bc, bd) = (
         Recursive(a) * Recursive(c),
         Recursive(a) * Recursive(d),
         Recursive(b) * Recursive(c),
         Recursive(b) * Recursive(d))
-      (ac tenPower (len / 2 * 2)) + ((ad + bc) tenPower(len / 2)) + bd
-    }
+      (ac tenPower (len / 2 * 2)) + ((ad + bc) tenPower (len / 2)) + bd
   }
 
   override def toString: String = n.toString
