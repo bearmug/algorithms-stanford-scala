5655deef78ee473084726e81e7f685a7bd1b7d9f
bearmug <pavel.fadeev@gmail.com>
2017-02-02 22:53:40 +0300

    #6 prepare for parallel version implementation

--- src/main/scala/org/bearmug/algo/course01/week01/Merge.scala
+++ src/main/scala/org/bearmug/algo/course01/week01/Merge.scala
@@ -2,27 +2,27 @@ package org.bearmug.algo.course01.week01
 
 import scala.collection.immutable.Nil
 
-class Merge[T](data: Seq[T]) {
+class Merge[T](data: Seq[T], ord: Ordering[T]) {
 
-  def sort(ord: Ordering[T]) = {
-    def merge(l: List[T], r: List[T]): List[T] = (l, r) match {
-      case (Nil, _) => r
-      case (_, Nil) => l
-      case (lH :: lT, rH :: rT) => if (ord.lteq(lH, rH)) lH :: merge(lT, r) else rH :: merge(l, rT)
-    }
-
-    def sort(seq: Seq[T]): List[T] = seq match {
-      case Nil => Nil
-      case s :: Nil => List(s)
-      case _ => seq.splitAt(seq.length / 2) match {
-        case (l, r) => merge(sort(l), sort(r))
-      }
-    }
-
-    sort(data)
+  def merge(l: List[T], r: List[T]): List[T] = (l, r) match {
+    case (Nil, _) => r
+    case (_, Nil) => l
+    case (lH :: lT, rH :: rT) => if (ord.lteq(lH, rH)) lH :: merge(lT, r) else rH :: merge(l, rT)
   }
+
+  def sort(seq: Seq[T], parLevels: Int): List[T] = seq match {
+    case Nil => Nil
+    case s :: Nil => List(s)
+    case _ => seq.splitAt(seq.length / 2) match {
+      case (l, r) => merge(sort(l, parLevels - 1), sort(r, parLevels - 1))
+    }
+  }
+
+  def sort(): Seq[T] = sort(data, 0)
+
+  def sortPar(parLevels: Int): Seq[T] = sort(data, parLevels)
 }
 
 object Merge {
-  def apply[T](data: Seq[T]) = new Merge(data)
+  def apply[T](data: Seq[T], ord: Ordering[T]) = new Merge(data, ord)
 }
--- src/test/scala/org/bearmug/algo/course01/week01/MergeSuite.scala
+++ src/test/scala/org/bearmug/algo/course01/week01/MergeSuite.scala
@@ -7,50 +7,50 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class MergeSuite extends FunSuite {
   test("sort works for empty input") {
-    assert(Merge(Nil).sort(Ordering.Int) == Nil)
+    assert(Merge(Nil, Ordering.Int).sort() == Nil)
   }
 
   test("sort works for single element input") {
-    assert(Merge(List(1)).sort(Ordering.Int) == List(1))
-    assert(Merge(List(-23)).sort(Ordering.Int) == List(-23))
-    assert(Merge(List(Int.MaxValue)).sort(Ordering.Int) == List(Int.MaxValue))
+    assert(Merge(List(1), Ordering.Int).sort() == List(1))
+    assert(Merge(List(-23), Ordering.Int).sort() == List(-23))
+    assert(Merge(List(Int.MaxValue), Ordering.Int).sort() == List(Int.MaxValue))
   }
 
   test("sort works for 2^n element input") {
-    assert(Merge(List(2, 1)).sort(Ordering.Int) == List(1, 2))
-    assert(Merge(List(1, 4, 6, 3)).sort(Ordering.Int) == List(1, 3, 4, 6))
-    assert(Merge(List(4, 6, 1, 8, 45, 2, 5, 6)).sort(Ordering.Int) == List(1, 2, 4, 5, 6, 6, 8, 45))
-    assert(Merge(List(4, 6, 1, 34, -1, 5555, 65, 8, 45, 2, 5, 6, 44, 777, 888, 99)).sort(Ordering.Int) ==
+    assert(Merge(List(2, 1), Ordering.Int).sort() == List(1, 2))
+    assert(Merge(List(1, 4, 6, 3), Ordering.Int).sort() == List(1, 3, 4, 6))
+    assert(Merge(List(4, 6, 1, 8, 45, 2, 5, 6), Ordering.Int).sort() == List(1, 2, 4, 5, 6, 6, 8, 45))
+    assert(Merge(List(4, 6, 1, 34, -1, 5555, 65, 8, 45, 2, 5, 6, 44, 777, 888, 99), Ordering.Int).sort() ==
       List(-1, 1, 2, 4, 5, 6, 6, 8, 34, 44, 45, 65, 99, 777, 888, 5555))
   }
 
   test("sort works for even element input") {
-    assert(Merge(List(2, 1, 554, -1, 33, 3)).sort(Ordering.Int) == List(-1, 1, 2, 3, 33, 554))
-    assert(Merge(List(4, 6, 1, 8, 45, 2, 5, 6, 45, 5)).sort(Ordering.Int) ==
+    assert(Merge(List(2, 1, 554, -1, 33, 3), Ordering.Int).sort() == List(-1, 1, 2, 3, 33, 554))
+    assert(Merge(List(4, 6, 1, 8, 45, 2, 5, 6, 45, 5), Ordering.Int).sort() ==
       List(1, 2, 4, 5, 5, 6, 6, 8, 45, 45))
-    assert(Merge(List(4, 6, 1, 34, -1, 5555, 65, 8, 45, 2, 5, 6, 44, 777)).sort(Ordering.Int) ==
+    assert(Merge(List(4, 6, 1, 34, -1, 5555, 65, 8, 45, 2, 5, 6, 44, 777), Ordering.Int).sort() ==
       List(-1, 1, 2, 4, 5, 6, 6, 8, 34, 44, 45, 65, 777, 5555))
   }
 
   test("sort works for odd element input") {
-    assert(Merge(List(-123, 2, 1, 554, -1, 33, 3)).sort(Ordering.Int) == List(-123, -1, 1, 2, 3, 33, 554))
-    assert(Merge(List(4, 6, 1, 8, 45, 2, 5, 6, 45, 5, -1000)).sort(Ordering.Int) ==
+    assert(Merge(List(-123, 2, 1, 554, -1, 33, 3), Ordering.Int).sort() == List(-123, -1, 1, 2, 3, 33, 554))
+    assert(Merge(List(4, 6, 1, 8, 45, 2, 5, 6, 45, 5, -1000), Ordering.Int).sort() ==
       List(-1000, 1, 2, 4, 5, 5, 6, 6, 8, 45, 45))
-    assert(Merge(List(4, 6, 1, 34, -1, 5555, 65, 8, 45, 2, 5, 6, 44, 777, 3)).sort(Ordering.Int) ==
+    assert(Merge(List(4, 6, 1, 34, -1, 5555, 65, 8, 45, 2, 5, 6, 44, 777, 3), Ordering.Int).sort() ==
       List(-1, 1, 2, 3, 4, 5, 6, 6, 8, 34, 44, 45, 65, 777, 5555))
   }
 
   test("sort with reverse order achievable") {
-    assert(Merge(List(2, 1, 554, -1, 33, 3)).sort(Ordering.Int.reverse) == List(554, 33, 3, 2, 1, -1))
+    assert(Merge(List(2, 1, 554, -1, 33, 3), Ordering.Int.reverse).sort() == List(554, 33, 3, 2, 1, -1))
   }
 
   test("sort works for strings") {
-    assert(Merge(List("asq", "a", "23, ", "yTR")).sort(Ordering.String) ==
+    assert(Merge(List("asq", "a", "23, ", "yTR"), Ordering.String).sort() ==
       List("23, ", "a", "asq", "yTR"))
   }
 
   test("sort works for characters") {
-    assert(Merge(List('e', 'q', '1', '%')).sort(Ordering.Char) ==
+    assert(Merge(List('e', 'q', '1', '%'), Ordering.Char).sort() ==
       List('%', '1', 'e', 'q'))
   }
 }
