6f454234db641a316257b4725d1050729869c13f
bearmug <pavel.fadeev@gmail.com>
2017-02-02 22:32:01 +0300

    #6 reverse order sorting achievable

--- src/test/scala/org/bearmug/algo/course01/week01/MergeSuite.scala
+++ src/test/scala/org/bearmug/algo/course01/week01/MergeSuite.scala
@@ -6,17 +6,17 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class MergeSuite extends FunSuite {
-  test("merge works for empty input") {
+  test("sort works for empty input") {
     assert(Merge(Nil).sort((a, b) => a <= b) == Nil)
   }
 
-  test("merge works for single element input") {
+  test("sort works for single element input") {
     assert(Merge(List(1)).sort((a, b) => a <= b) == List(1))
     assert(Merge(List(-23)).sort((a, b) => a <= b) == List(-23))
     assert(Merge(List(Int.MaxValue)).sort((a, b) => a <= b) == List(Int.MaxValue))
   }
 
-  test("merge works for 2^n element input") {
+  test("sort works for 2^n element input") {
     assert(Merge(List(2, 1)).sort((a, b) => a <= b) == List(1, 2))
     assert(Merge(List(1, 4, 6, 3)).sort((a, b) => a <= b) == List(1, 3, 4, 6))
     assert(Merge(List(4, 6, 1, 8, 45, 2, 5, 6)).sort((a, b) => a <= b) == List(1, 2, 4, 5, 6, 6, 8, 45))
@@ -24,7 +24,7 @@ class MergeSuite extends FunSuite {
       List(-1, 1, 2, 4, 5, 6, 6, 8, 34, 44, 45, 65, 99, 777, 888, 5555))
   }
 
-  test("merge works for even element input") {
+  test("sort works for even element input") {
     assert(Merge(List(2, 1, 554, -1, 33, 3)).sort((a, b) => a <= b) == List(-1, 1, 2, 3, 33, 554))
     assert(Merge(List(4, 6, 1, 8, 45, 2, 5, 6, 45, 5)).sort((a, b) => a <= b) ==
       List(1, 2, 4, 5, 5, 6, 6, 8, 45, 45))
@@ -32,11 +32,15 @@ class MergeSuite extends FunSuite {
       List(-1, 1, 2, 4, 5, 6, 6, 8, 34, 44, 45, 65, 777, 5555))
   }
 
-  test("merge works for odd element input") {
+  test("sort works for odd element input") {
     assert(Merge(List(-123, 2, 1, 554, -1, 33, 3)).sort((a, b) => a <= b) == List(-123, -1, 1, 2, 3, 33, 554))
     assert(Merge(List(4, 6, 1, 8, 45, 2, 5, 6, 45, 5, -1000)).sort((a, b) => a <= b) ==
       List(-1000, 1, 2, 4, 5, 5, 6, 6, 8, 45, 45))
     assert(Merge(List(4, 6, 1, 34, -1, 5555, 65, 8, 45, 2, 5, 6, 44, 777, 3)).sort((a, b) => a <= b) ==
       List(-1, 1, 2, 3, 4, 5, 6, 6, 8, 34, 44, 45, 65, 777, 5555))
+  }
+
+  test("sort with reverse order achievable") {
+    assert(Merge(List(2, 1, 554, -1, 33, 3)).sort((a, b) => a >= b) == List(554, 33, 3, 2, 1, -1))
   }
 }
