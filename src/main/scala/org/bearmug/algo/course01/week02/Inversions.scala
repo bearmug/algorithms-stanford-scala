c52380ecde31b7ecf8fc559cee5381443fd471fb
bearmug <pavel.fadeev@gmail.com>
2017-02-03 20:55:05 +0300

    #8 inversions base class submitted

---
+++ src/main/scala/org/bearmug/algo/course01/week02/Inversions.scala
@@ -0,0 +1,5 @@
+package org.bearmug.algo.course01.week02
+
+class Inversions {
+
+}
---
+++ src/test/scala/org/bearmug/algo/course01/week02/InversionsSuite.scala
@@ -0,0 +1,10 @@
+package org.bearmug.algo.course01.week02
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class InversionsSuite extends FunSuite {
+
+}
