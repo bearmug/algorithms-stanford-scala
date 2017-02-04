58ad9d4b4f5cdd092ec034c91308866a8a265a77
bearmug <pavel.fadeev@gmail.com>
2017-02-04 12:12:51 +0300

    #8 proof that simple inversions count does not work and provide draft for mergesort version

--- src/main/scala/org/bearmug/algo/course01/week02/Inversions.scala
+++ src/main/scala/org/bearmug/algo/course01/week02/Inversions.scala
@@ -1,5 +1,48 @@
 package org.bearmug.algo.course01.week02
 
-class Inversions {
+import scala.collection.immutable.Nil
 
+class Inversions(data: List[Int]) {
+
+  type Inv = (List[Int], Int)
+
+  def merge(l: List[Int], r: List[Int], acc: Int): Inv = (l, r) match {
+    case (Nil, _) => (Nil, acc)
+    case (_, Nil) => (Nil, acc)
+    case ((lH :: lT), (rH :: rT)) =>
+      if (lH <= rH) {
+        val res = merge(lT, r, acc)
+        (lH :: res._1, res._2)
+      } else {
+        val res = merge(l, rT, acc + l.length)
+        (rH :: res._1, res._2)
+      }
+  }
+
+  def inversions(seq: Seq[Int]): Inv = seq match {
+    case Nil => (Nil, 0)
+    case _ :: Nil => (Nil, 0)
+    case _ => seq.splitAt(seq.length / 2) match {
+      case (l, r) => {
+        val invL = inversions(l)
+        val invR = inversions(r)
+        merge(invL._1, invR._1, invL._2 + invR._2)
+      }
+    }
+  }
+
+  def inversionsSimple(): Int = inversions(data)._2
+
+  def inversionsPar(): Int = ???
+
+  /**
+    * Invalid approach since we have may not track full solution tree by this
+    */
+  def inversionsFast(): Int = data.zipWithIndex.map {
+    case (e, index) => (e max index + 1) - (index + 1)
+  }.sum
+}
+
+object Inversions {
+  def apply(data: List[Int]) = new Inversions(data)
 }
--- src/test/scala/org/bearmug/algo/course01/week02/InversionsSuite.scala
+++ src/test/scala/org/bearmug/algo/course01/week02/InversionsSuite.scala
@@ -7,4 +7,23 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class InversionsSuite extends FunSuite {
 
+  test("inversionsFast works for empty input") {
+    assert(Inversions(List[Int]()).inversionsFast() == 0)
+  }
+
+  test("inversionsFast works for single element") {
+    assert(Inversions(List(1)).inversionsFast() == 0)
+  }
+
+  test("inversionsFast works for non-inversed sequence") {
+    assert(Inversions(List(1, 2, 3, 4, 5, 6)).inversionsFast() == 0)
+  }
+
+  test("inversionsFast works for two inversed elements") {
+    assert(Inversions(List(2, 1)).inversionsFast() == 1)
+  }
+
+  test("inversionsFast failed for of three two inversed elements") {
+    assert(Inversions(List(3, 2, 1)).inversionsFast() != 3)
+  }
 }
