46f016dd7a454cb4e16736d1340cc71609847330
bearmug <pavel.fadeev@gmail.com>
2017-02-12 00:39:42 +0300

    #10 increment functional style usage with tailrec call

--- src/main/scala/org/bearmug/algo/course01/week03/QuickSort.scala
+++ src/main/scala/org/bearmug/algo/course01/week03/QuickSort.scala
@@ -1,6 +1,6 @@
 package org.bearmug.algo.course01.week03
 
-import scala.math.Ordering
+import scala.annotation.tailrec
 
 class QuickSort(data: Vector[Int]) {
 
@@ -10,23 +10,31 @@ class QuickSort(data: Vector[Int]) {
   type Index = Int
 
   /**
-    * Data vector and its left (inclusive) and right (exclusive) border
+    * Data vector and its
     */
-  type Stripe = (Vector[Int], Index, Index)
+  type Data = Vector[Int]
 
-  private def sort(data: Stripe, pivotIndex: (Stripe) => Index): Vector[Int] = {
-    def swap(data: Stripe): Vector[Int] = data match {
-      case (v, s1, s2) if s1 == s2 => v
-      case (v, s1, s2) => {
-        v.updated(s1, v(s2)).updated(s2, v(s1))
+  /**
+    * Data range to process to: left (inclusive) and right (non-inclusive) border
+    */
+  type Stripe = (Index, Index)
+
+  @tailrec
+  private def sort(d: Data, s: List[Stripe], pivotIndex: (Data, Stripe) => Index): Data = {
+    def swap(d: Data, s1: Index, s2: Index): Data = (s1, s2) match {
+      case (no, swap) if no == swap => d
+      case (_, _) => {
+        d.updated(s1, d(s2)).updated(s2, d(s1))
       }
     }
 
-    data match {
-      case (v, l, r) if l + 1 >= r => v
-      case (v, l, r) => {
-        var dataInWork = swap(v, l, pivotIndex(v, l, r))
-        var nextLess = l + 1 //- next position for element smaller than pivot
+    (d, s) match {
+      case (dt, Nil) => dt
+      case (dt, (l, r) :: tail) if l + 1 >= r => sort(dt, tail, pivotIndex)
+      case (dt, (l, r) :: tail) => {
+        var dataInWork = swap(dt, l, pivotIndex(dt, (l, r)))
+        var nextLess = l + 1
+        //- next position for element smaller than pivot
         var nextMore = l + 1 //- next position for element bigger than pivot
         for (i <- l + 1 until r) {
           dataInWork(i) match {
@@ -40,21 +48,33 @@ class QuickSort(data: Vector[Int]) {
             }
           }
         }
-        val presorted = swap(dataInWork, l, nextLess - 1)
-        val sortedLeft = sort((presorted, l, nextLess - 1), pivotIndex)
-        sort((sortedLeft, nextLess, r), pivotIndex)
+        var head = tail
+        if (l < nextLess - 2) {
+          head = (l, nextLess - 1) :: head
+        }
+        if (nextLess < r - 1) {
+          head = (nextLess, r) :: head
+        }
+        sort(swap(dataInWork, l, nextLess - 1), head, pivotIndex)
       }
     }
   }
 
-  private def sort(pivotIndex: (Stripe) => Index): Vector[Int] = sort((data, 0, data.length), pivotIndex)
+  private def sort(pivotIndex: (Data, Stripe) => Index): Data =
+    sort(data, (0, data.length) :: Nil, pivotIndex)
 
-  def sortFirst(): Vector[Int] = sort((s) => s._2)
-  def sortLast(): Vector[Int] = sort((s) => s._3 - 1  )
-  def sortMedian(): Vector[Int] = sort((s) => s match {
-    case (v, l, r) => {
+  def sortFirst(): Vector[Int] = sort((_, r) => r match {
+    case (i, _) => i
+  })
+
+  def sortLast(): Vector[Int] = sort((_, r) => r match {
+    case (_, i) => i - 1
+  })
+
+  def sortMedian(): Vector[Int] = sort((d, s) => (d, s) match {
+    case (dt, (l, r)) => {
       val medianIndex = (l + r - 1) / 2
-      Vector((l, v(l)), (r - 1, v(r - 1)), (medianIndex, v(medianIndex))).sortBy(_._2) match {
+      Vector((l, dt(l)), (r - 1, dt(r - 1)), (medianIndex, dt(medianIndex))).sortBy(_._2) match {
         case Vector(_, (idx, _), _) => idx
       }
     }
