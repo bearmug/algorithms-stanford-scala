020a6bf4e6ff151ffcbe8d70306a0af8f96c701d
bearmug <pavel.fadeev@gmail.com>
2017-03-05 02:44:03 +0300

    #18 dummy test submission

---
+++ src/main/scala/org/bearmug/algo/course02/week02/Dijkstra.scala
@@ -0,0 +1,10 @@
+package org.bearmug.algo.course02.week02
+
+
+class Dijkstra {
+
+}
+
+object Dijkstra {
+  def apply(): Dijkstra = new Dijkstra()
+}
---
+++ src/test/scala/org/bearmug/algo/course02/week02/DijkstraSuite.scala
@@ -0,0 +1,13 @@
+package org.bearmug.algo.course02.week02
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class DijkstraSuite extends FunSuite {
+
+  test("dummy test") {
+    assert(Dijkstra().isInstanceOf[Dijkstra])
+  }
+}
