0b773043876f652492cfd09b313033b68ed8cc49
bearmug <pavel.fadeev@gmail.com>
2017-03-05 20:07:38 +0300

    #18 JMH harness introduction

---
+++ src/jmh/scala/org/bearmug/algo/course02/week02/DijkstraBench.scala
@@ -0,0 +1,33 @@
+package org.bearmug.algo.course02.week02
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+
+import scala.util.Random
+
+/**
+  * Benchmark                            Mode  Cnt  Score   Error  Units
+  *DijkstraBench.shortestPaths         thrpt    5  1.479 ± 1.204  ops/s
+  */
+@State(Scope.Benchmark)
+@Warmup(iterations = 2, time = 5, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 3, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 1)
+class DijkstraBench {
+
+  var gr: Map[Int, List[(Int, Int)]] = Map.empty
+
+  @Setup
+  def setup() = {
+    gr = (1 to 10000).map { _ =>
+      Random.nextInt(1000) ->
+        (1 to 30).map { n =>
+          (Random.nextInt(1000), Random.nextInt(1000))
+        }.toList
+    }.toMap
+  }
+
+  @Benchmark
+  def shortestPaths(): List[(Int, Int)] = Dijkstra(gr).shortestPaths(gr.keySet.head)
+}
