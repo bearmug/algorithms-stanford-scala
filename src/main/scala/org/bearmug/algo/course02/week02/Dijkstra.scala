3257a7ccf6253d1ce200aabaf59d00acb9981d65
bearmug <pavel.fadeev@gmail.com>
2017-03-05 17:31:17 +0300

    #18 straightforward dijkstra implementation

--- src/main/scala/org/bearmug/algo/course02/week02/Dijkstra.scala
+++ src/main/scala/org/bearmug/algo/course02/week02/Dijkstra.scala
@@ -1,19 +1,27 @@
 package org.bearmug.algo.course02.week02
 
+import scala.annotation.tailrec
 
-class Dijkstra private(map: Map[Int, List[(Int, Int)]]) {
+
+class Dijkstra private(m: Map[Int, List[(Int, Int)]]) {
 
   type M = Map[Int, List[(Int, Int)]]
   type N = List[(Int, Int)]
 
   def shortestPaths(source: Int): N = {
 
+    @tailrec
     def shortestPaths(frontier: N, acc: N): N = frontier match {
-      case List() => acc
-      case _ => ???
+      case Nil => acc
+      case (v, cost) :: tail => {
+        val increment = m(v)
+          .filter(n => !acc.map(_._1).toSet.contains(n))
+          .map(n => (n._1, n._2 + cost))
+        shortestPaths((increment ::: tail).sortBy(_._2).reverse, increment ::: acc)
+      }
     }
 
-    shortestPaths(map(source), List.empty)
+    shortestPaths(m(source).sortBy(_._2).reverse, List.empty)
   }
 
 }
