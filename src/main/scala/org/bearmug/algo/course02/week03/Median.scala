c7ab8c0e121fa0ac74de92c7ca3fcd44ba137772
bearmug <pavel.fadeev@gmail.com>
2017-03-13 23:15:45 +0300

    #22 build scalable solution

--- src/main/scala/org/bearmug/algo/course02/week03/Median.scala
+++ src/main/scala/org/bearmug/algo/course02/week03/Median.scala
@@ -5,30 +5,36 @@ import scala.collection.immutable.{SortedSet, TreeSet}
 /**
   * Created by pavel on 3/12/17.
   */
-class Median private(left: SortedSet[Int], right: SortedSet[Int]) {
+class Median private(lHeap: SortedSet[Int], rHeap: SortedSet[Int]) {
 
   type H = SortedSet[Int]
 
   def +(e: Int): Median = {
 
-    def balance(l: H, r: H): Median = (l.size, r.size) match {
-      case (lS, rS) if lS > rS => new Median(l.tail, r + l.head)
-      case (lS, rS) if lS < rS => new Median(l + r.head, r.tail)
-      case (_, _) => new Median(l, r)
+    def balance(l: H, r: H): (H, H) = (l.size, r.size) match {
+      case (lS, rS) if lS > rS => (l.tail, r + l.head)
+      case (lS, rS) if lS + 1 < rS => (l + r.head, r.tail)
+      case (_, _) => (l, r)
     }
 
     // use currying for explicit branching
-    def add(e: Int)(f: (H, H) => Median): Median = (left.headOption, right.headOption) match {
-      case (None, _) => f(left + e, right)
-      case (_, None) => f(left, right + e)
-      case (lH, _) if e <= lH.get => f(left + e, right)
-      case (_, _) => f(left, right + e)
+    def add(e: Int)(f: (H, H) => (H, H)): (H, H) =
+      (lHeap.headOption, rHeap.headOption) match {
+      case (_, None) => f(lHeap, rHeap + e)
+      case (_, Some(rHead)) if e >= rHead => f(lHeap, rHeap + e)
+      case (_, _) => f(lHeap + e, rHeap)
     }
 
-    add(e)(balance)
+    Median(add(e)(balance))
+  }
+
+  def calc: Int = rHeap.headOption match {
+    case Some(m) => m
+    case _ => Int.MaxValue
   }
 }
 
 object Median {
-  def apply(): Median = new Median(TreeSet.empty(Ordering.Int), TreeSet.empty(Ordering.Int.reverse))
+  def apply(): Median = new Median(TreeSet.empty(Ordering.Int.reverse), TreeSet.empty(Ordering.Int))
+  private def apply(lr: (SortedSet[Int], SortedSet[Int])) = new Median(lr._1, lr._2)
 }
--- src/test/scala/org/bearmug/algo/course02/week03/MedianSuite.scala
+++ src/test/scala/org/bearmug/algo/course02/week03/MedianSuite.scala
@@ -4,7 +4,51 @@ import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
+import scala.util.Random
+
 @RunWith(classOf[JUnitRunner])
 class MedianSuite extends FunSuite {
-  test("calc works for empty data") { }
+
+  test("calc works for empty data") {
+    assert(Median().calc == Int.MaxValue)
+  }
+
+  test("calc works for single element") {
+    assert((Median() + 1).calc == 1)
+  }
+
+  test("calc works for two elements") {
+    assert((Median() + 1 + 3).calc == 3)
+    assert((Median() + 5 + 3).calc == 5)
+  }
+
+  test("calc works for two same elements") {
+    assert((Median() + 1 + 1).calc == 1)
+    assert((Median() + 4 + 1 + 3 + 3).calc == 3)
+  }
+
+  test("calc works for three elements") {
+    assert((Median() + 1 + 3 + 2).calc == 2)
+    assert((Median() + 3 + 2 + 1).calc == 2)
+    assert((Median() + 2 + 3 + 1).calc == 2)
+  }
+
+  test("calc works for four elements") {
+    assert((Median() + 1 + 3 + 2 + 4).calc == 3)
+    assert((Median() + 5 + 3 + 4 + 2).calc == 4)
+  }
+
+  test("calc works for five elements") {
+    assert((Median() + 1 + 3 + 2 + 4 + 5).calc == 3)
+    assert((Median() + 5 + 3 + 4 + 2 + 1).calc == 3)
+  }
+
+  test("calc works for even elements") {
+    for (i <- 2 to 1000){
+      val data: Vector[Int] = Seq.fill(i)(Random.nextInt(100000)).toSet.toVector
+      assert(data.sorted.apply(data.length/2) == data.foldLeft(Median())((m, i) => m + i).calc,
+        s", \nlength: $i," +
+          s"\ninput: $data")
+    }
+  }
 }
